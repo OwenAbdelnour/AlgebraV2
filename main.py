bf6c4d9f80754e4a15ca943d8dcd33fc326c47b9
Owen Abdelnour <97808436+OwenAbdelnour@users.noreply.github.com>
2022-01-21 20:54:53 +0000

    Update

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 equation = input("Equation: ")
 
-terms = [[], []]
+terms = [[[[0]]], [[[0]]]]
 x = 0
 a = 0
-co = 0
 par_signs = []
 # 2x(3+3)/10+9
 # [[2, "x"], [[3], [3]], [10], "*/"], [9]
@@ -22,36 +21,90 @@ while a < len(equation):
   else:
     x = 0
 
+  # Modifier Control
+  if isinstance(terms[x][-1][-1], list):
+    mc = -1
+  else:
+    mc = -2
+
+  # +-
   if equation[a] in "+-":
+    # New term
     if equation[a-1] not in "+-*/(":
-      terms[x].append([0])
+      terms[x].append([[0]])
+    # Negative sign
     if equation[a]=="-":
-      if isinstance(terms[x][-1][-1], str):
-        terms[x][-1][-2][0] *= -1
+      if terms[x][-1][mc][0] != "-":
+        terms[x][-1][mc][0] = "-"
       else:
-        terms[x][-1][-1][0] *= -1
+        terms[x][-1][mc][0] = 0
   
+  # */
   elif equation[a] in "*/":
+    terms[x][-1].append([0])
     if isinstance(terms[x][-1][-1], list):
       terms[x][-1].append("")
     terms[x][-1][-1] += equation[a]
 
+  # (
   elif equation[a]=="(":
     # 3-(1+1)
     if equation[a-1]=="-":
-      term[x].append([[-1], "*"])
+      terms[x].append([[-1], "*"])
+      par_signs.append("*")
+    # 3*(1+1) | 6/(1+1)
+    elif equation[a-1] in "*/":
+      del terms[x][-1][-2]
+      par_signs.append(equation[a-1])
     # 2(2+1)
-    elif equation[a-1] not in "+*/":
+    elif equation[a-1] != "+":
       if isinstance(terms[x][-1][-1], list):
         terms[x][-1].append("")
-      terms[x][-1][-1] += equation[a]
+      terms[x][-1][-1] += "*"
+      par_signs.append("*")
     # New partition
-    terms.append([])
-    # Save Sign
-    par_signs.append(equation[a-1])
+    terms.append([[[0]]])
   
+  # )
   elif equation[a]==")":
-    
+    # Find term to add partition
+    if len(terms)>3 or equation.find("=")>a:
+      y = -2
+    else:
+      y = 0
+    # Add partition based off saved sign
     if par_signs[-1] in "-*/":
-      terms
+      terms[y][-1].insert(-1, terms[-1])
+    else:
+      terms[y].append(terms[-1])
+    # Delete partition and saved sign
+    del par_signs[-1]
+    del terms[-1]
+
+  # Numbers and Varibles
+  else:
+    # Numbers
+    if equation[a] in ".0123456789":
+      terms[x][-1][mc][0] = int(str(terms[x][-1][mc][0])+equation[a])
+    # Varibles
+    else:
+      if len(terms[x][-1][mc])==1:
+        terms[x][-1][mc].append("")
+      terms[x][-1][mc][1] += equation[a]
   a += 1
+  print(terms)
+
+def display():
+  eq = []
+  def inner(term):
+    pass
+
+  # Main Loop
+  for term in terms:
+    inner(term)
+
+  # Print Loop
+  for i in eq:
+    print(i, end="")
+  print()
+display()
